11365eb332d106f94e5e69b8fc376197492f63ea
Omer Priel <omer2079@gmail.com>
2020-12-05 23:37:10 +0200

    import Door assets

--- Source/BuildingEscape/Components/OpenDoorComponent.cpp
+++ Source/BuildingEscape/Components/OpenDoorComponent.cpp
@@ -8,8 +8,6 @@ UOpenDoorComponent::UOpenDoorComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -30,7 +28,5 @@ void UOpenDoorComponent::BeginPlay()
 void UOpenDoorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
